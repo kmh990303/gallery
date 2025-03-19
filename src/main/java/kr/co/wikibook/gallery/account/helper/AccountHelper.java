2d9fb2c3bd462279746cc637ebe6b34b6a92df73
Minhyuk Kim <labcs@naver.com>
2025-03-19 21:15:07 +0900

    멤버 서비스 구현

---
+++ src/main/java/kr/co/wikibook/gallery/account/helper/AccountHelper.java
@@ -0,0 +1,7 @@
+package kr.co.wikibook.gallery.account.helper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface AccountHelper {
+}
---
+++ src/main/java/kr/co/wikibook/gallery/member/entity/Member.java
@@ -0,0 +1,37 @@
+package kr.co.wikibook.gallery.member.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String loginId;
+
+    @Column(length = 100, nullable = false)
+    private String loginPw;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime created;
+
+    public Member(String name, String loginId, String loginPw) {
+        this.name = name;
+        this.loginId = loginId;
+        this.loginPw = loginPw;
+    }
+}
