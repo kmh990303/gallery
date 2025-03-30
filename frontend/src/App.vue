e7de2479236116a550728f2eae60977d3aad4764
Minhyuk Kim <labcs@naver.com>
2025-03-30 15:24:57 +0900

    App.vue 컴포넌트 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import {useAccountStore} from "@/stores/account";
+import {watch} from "vue";
+import {useRoute} from "vue-router";
+
+const accountStore = useAccountStore();
+
+const route = useRoute();
+
+const checkAccount = async () => {
+
+}
+
+(async function onCreated() {
+  await checkAccount();
+})();
+
+watch(() => route.path, () => {
+  checkAccount();
+})
 </script>
 
 <template>
   <Header />
+  <main>
+    <router-view></router-view>
+  </main>
   <Footer />
 </template>
