7a0407a472ebd9b7ef44d61162c270d585f23c6c
Minhyuk Kim <labcs@naver.com>
2025-04-02 21:31:24 +0900

    주문 관련 기능 구현

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -22,6 +22,21 @@ const router = createRouter({
             path: '/cart',
             name: 'cart',
             component: () => import("../views/Cart.vue")
+        },
+        {
+            path: '/order',
+            name: 'orderForm',
+            component: () => import('../views/OrderForm.vue')
+        },
+        {
+            path: '/orders',
+            name: 'orders',
+            component: () => import('../views/Orders.vue')
+        },
+        {
+            path: '/orders/:id',
+            name: 'orderDetail',
+            component: () => import('../views/OrderDetail.vue')
         }
     ]
 })
