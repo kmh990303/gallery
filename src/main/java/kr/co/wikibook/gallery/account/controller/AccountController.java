72c20f86d01fb49f4d04576663dae334eb623774
Minhyuk Kim <labcs@naver.com>
2025-04-05 19:30:32 +0900

    백엔드 액세스토큰 리프레시토큰 로직 작성

--- src/main/java/kr/co/wikibook/gallery/account/controller/AccountController.java
+++ src/main/java/kr/co/wikibook/gallery/account/controller/AccountController.java
@@ -6,16 +6,24 @@ import kr.co.wikibook.gallery.account.dto.AccountJoinRequest;
 import kr.co.wikibook.gallery.account.dto.AccountLoginRequest;
 import kr.co.wikibook.gallery.account.helper.AccountHelper;
 import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.Token;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import kr.co.wikibook.gallery.common.util.TokenUtils;
+import kr.co.wikibook.gallery.block.service.BlockService;
+import kr.co.wikibook.gallery.common.util.HttpUtils;
+import kr.co.wikibook.gallery.account.etc.AccountConstants;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1")
 public class AccountController {
     private final AccountHelper accountHelper;
+    private final BlockService blockService;
 
     @PostMapping("/api/account/join")
     public ResponseEntity<?> join(@RequestBody AccountJoinRequest joinReq) {
@@ -55,5 +63,21 @@ public class AccountController {
     public ResponseEntity<?> logout(HttpServletRequest req, HttpServletResponse res) {
         accountHelper.logout(req, res);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/api/account/token")
+    public ResponseEntity<?> regenerate(HttpServletRequest req) {
+        String accessToken = "";
+        String refreshToken = HttpUtils.getCookieValue(req, AccountConstants.REFRESH_TOKEN_NAME);
+
+        if (StringUtils.hasLength(refreshToken) && TokenUtils.isValid(refreshToken) && !blockService.has(refreshToken)) {
+            Map<String, Object> tokenBody = TokenUtils.getBody(refreshToken);
+
+            Integer memberId = (Integer) tokenBody.get(AccountConstants.MEMBER_ID_NAME);
+
+            accessToken = TokenUtils.generate(AccountConstants.ACCESS_TOKEN_NAME, AccountConstants.MEMBER_ID_NAME, memberId, AccountConstants.ACCESS_TOKEN_EXP_MINUTES);
+        }
+
+        return new ResponseEntity<>(accessToken, HttpStatus.OK);
     }
 }
